f5e760f7e0f0de27ecc4a9ff5c958eed9ea4bc9e
Nagesh Mandal <106006006+NageshMandal@users.noreply.github.com>
2022-11-29 21:15:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// var tabButtons = document.querySelectorAll(".tabcontainer .buttoncontainer button");
+// var tabpanles = document.querySelectorAll(".tabcontainer .tabpanel")
+
+// function showPanel(panelIndex,colorCode) {
+//     tabButtons.forEach(function(node){
+//         node.style.backgroundcolour="";
+//         node.style.color="";
+//     });
+//     tabButtons[panelIndex].style.backgroundcolour=colorCode;
+//     tabButtons[panelIndex].style.color="white";
+//     tabButtons.forEach(function(node){
+//         node.style.display="none";
+//     });
+//     tabpanles[panelIndex].style.display="block";
+//     tabpanles[panelIndex].style.backgroundcolour=colorCode;
+// }
+// showPanel(0,'#f44336');
+
+
+
+
+
+
+
+
+
+
+
